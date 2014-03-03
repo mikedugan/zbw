545347e070f86091f48fdc40fb98e890e1d291b2
mike <admin@local.host>
2014-03-02 20:07:26 -0500

    tidy up the views

--- app/views/includes/_header.blade.php
+++ app/views/includes/_header.blade.php
@@ -1,5 +1,6 @@
-<a id="logo" href="{{ url_to('/') }}"><img src="images/zbw_logo.png"></a>
+<a id="logo" href="{{ url('/') }}"><img src="images/zbw_logo.png"></a>
 <ul class="col-md-4 nav navbar-nav navbar-left">
         <li class="left"><a href="{{  url('controllers') }}">Controllers</a></li>
         <li class="left"><a href="{{  url('pilots') }}">Pilots</a></li>
+        <li class="left">{{ HTML::link('forum', 'Forum') }}</li>
     </ul>
--- app/views/includes/nav/_user.blade.php
+++ app/views/includes/nav/_user.blade.php
@@ -1,11 +1,12 @@
 <ul class="nav navbar-nav navbar-right col-md-1">
     <li class="dropdown">
-        <a href="#" class="dropdown-toggle" data-toggle="dropdown">DA <b class="caret"></b></a>
+        <a href="#" class="dropdown-toggle" data-toggle="dropdown">{{ $me->initials }} <b class="caret"></b></a>
         <ul class="dropdown-menu">
-            <li><a href="#">Settings</a></li>
-            <li><a href="{{ url_to('training') }}">Training Center</a></li>
-            <li><a href="#">Messages</a></li>
-            <li><a href="#">Scheduler</a></li>
+            <li>{{ HTML::link($me->cid."/settings", 'Settings') }}</li>
+            <li>{{ HTML::link('training', 'Training Center') }}</li>
+            <li>{{ HTML::link($me->cid."/inbox", 'Messages') }}</li>
+            <li>{{ HTML::link('schedule', 'Scheduler') }}</li>
+            <li>{{ HTML::link('logout', 'Logout') }}</li>
         </ul>
     </li>
 </ul>
