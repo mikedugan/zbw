2f663399378a1376e842d824831ec038f3268961
mikedugan <mike@mjdugan.com>
2014-03-09 17:54:20 -0400

    modified uploads table

--- app/database/models/PrivateMessage.php
+++ app/database/models/PrivateMessage.php
@@ -3,5 +3,16 @@
 class PrivateMessage extends Eloquent {
 	protected $guarded = array();
 	protected $table = 'zbw_messages';
-	public static $rules = array();
+	public static $rules = [
+        'from' => '',
+        'to' => '',
+        'subject' => 'max:60'
+    ];
+
+    public function __construct()
+    {
+        $cids = \Zbw\Helpers::getCids(true);
+        $this->rules['from'] = 'in:' . $cids;
+        $this->rules['to'] = 'in:' . $cids;
+    }
 }
