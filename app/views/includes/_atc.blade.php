bcd5aa219675932bab59e035ca89a4aadd05c0af
mikedugan <mike@mjdugan.com>
2014-03-16 00:33:59 -0400

    cert table and notifications view

---
+++ app/views/includes/_atc.blade.php
@@ -0,0 +1,29 @@
+<div class="col-md-12 online">
+	<table class="table table-striped">
+		<thead>
+			<tr>
+				<th>Controller</th>
+				<th>Position</th>
+				<th>Frequency</th>
+				<th>Sign-On</th>
+				<th>Sign-Off</th>
+			</tr>
+		</thead>
+		<tbody>
+			<tr>
+				<td>Don Desfosse</td>
+				<td>BOS_APP</td>
+				<td>118.250</td>
+				<td>22:45 UTC</td>
+				<td>01:30 UTC</td>
+			</tr>
+			<tr>
+				<td>Mike Willey</td>
+				<td>BOS_CTR</td>
+				<td>134.700</td>
+				<td>21:35 UTC</td>
+				<td>23:45 UTC</td>
+      </tr>
+		</tbody>
+	</table>
+</div>
