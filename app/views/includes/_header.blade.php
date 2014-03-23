8f20f3ffa447ff5009c462d56eef938010a92685
mike <mike@internetconnection.net>
2014-03-23 13:42:20 -0400

    dropdown left nav

--- app/views/includes/_header.blade.php
+++ app/views/includes/_header.blade.php
@@ -1,6 +1,33 @@
 <a id="logo" href="{{ url('/') }}">{{ HTML::image('images/zbw_logo.png', 'vZBW ARTCC') }}</a>
 <ul class="col-md-6 nav navbar-nav navbar-left">
-        <li class="left"><a href="{{  url('controllers') }}">Controllers</a></li>
-        <li class="left"><a href="{{  url('pilots') }}">Pilots</a></li>
-        <li class="left">{{ HTML::link('forum', 'Forum') }}</li>
+    <li class="dropdown">
+        <a href="/controllers" class="dropdown-toggle" data-toggle="dropdown">Controllers <b class="caret"></b></a>
+        <ul class="dropdown-menu">
+            <li><a href="#">Airports & Charts</a></li>
+            <li><a href="#">Link A</a></li>
+            <li><a href="#">Link B</a></li>
+            <li><a href="#">Link C</a></li>
+            <li><a href="#">Link D</a></li>
+        </ul>
+    </li>
+    <li class="dropdown">
+        <a href="/pilots" class="dropdown-toggle" data-toggle="dropdown">Pilots <b class="caret"></b></a>
+        <ul class="dropdown-menu">
+            <li><a href="#">Airports</a></li>
+            <li><a href="#">Charts</a></li>
+            <li><a href="#">Events & News</a></li>
+            <li><a href="#">Resources</a></li>
+            <li><a href="#">Link</a></li>
+        </ul>
+    </li>
+    <li class="dropdown">
+        <a href="/forum" class="dropdown-toggle" data-toggle="dropdown">Forum <b class="caret"></b></a>
+        <ul class="dropdown-menu">
+            <li><a href="#">Home</a></li>
+            <li><a href="#">NOTAMS</a></li>
+            <li><a href="#">Pilots</a></li>
+            <li><a href="#">Controllers</a></li>
+            <li><a href="#">Off-Topic</a></li>
+        </ul>
+    </li>
     </ul>
