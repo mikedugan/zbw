a10bf9968673ce4b3b2ef2912cda7beecd595c90
mike <mike@mjdugan.com>
2014-05-01 19:40:15 -0400

    modal login

--- public/js/events.js
+++ public/js/events.js
@@ -1,8 +1,4 @@
 $(function() {
-	$('#nav-login a').click(function() {
-		$('#nav-login').children().toggleClass('hidden');
-	});
-
 	//this handles any forms that should be submitted to the Ajax Controller
 	//they -must- have a route in routes.php, action, and method
 	$('.axform').submit(function(e) {
