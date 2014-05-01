22d2a65b8abfe0d72959a69fbbe3eda7bffd14c5
mikedugan <mike@mjdugan.com>
2014-05-01 19:40:15 -0400

    modal login

--- app/views/includes/nav/_login.blade.php
+++ app/views/includes/nav/_login.blade.php
@@ -1,8 +1,6 @@
 <form action="/login" method="post">
 <ul id="nav-login" class="nav navbar-nav navbar-right col-md-5">
-    <li class="hidden">username <input type="text"  name="username"></li>
-    <li class="hidden">password <input type="password" name="password"></li>
-    <button type="submit" class="btn btnprimary hidden">login</button>
-    <li class="pull-right"><a href="#">Login</a></li>
+    <li class="pull-right"><a href="#" data-toggle="modal" data-target="#loginModal">Login</a></li>
 </ul>
 </form>
+@include('includes._modal_login')
