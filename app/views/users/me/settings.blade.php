5ed33d1d17cded9bb482a3119c984651b5e2e191
mikedugan <mike@mjdugan.com>
2014-05-01 20:20:13 -0400

    Trial of a little settings thing

--- app/views/users/me/settings.blade.php
+++ app/views/users/me/settings.blade.php
@@ -6,4 +6,10 @@ My Profile
 @stop
 @section('content')
 	<h1 class="text-center">My Profile</h1>
+    <div class="col-md-12">
+    <p><b>Full Name:</b></p>$me['name']</p>
+    <p><b>Controller ID:</b></p>$me['cid']</p>
+    <p><b>Operating Initials:</b></p>$me['operating_id']
+    <p><b>Email:</b></p><?php $me['email'] = Form::text('email', 'example@gmail.com');?>
+    Form::checkbox('Subscribe', 'value', true);
 @stop
