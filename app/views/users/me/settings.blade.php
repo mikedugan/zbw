0340c49b294150edef638f42010292b0b9fbc540
mike <mike@mjdugan.com>
2014-05-01 20:35:05 -0400

    settings view

--- app/views/users/me/settings.blade.php
+++ app/views/users/me/settings.blade.php
@@ -7,9 +7,16 @@ My Profile
 @section('content')
 	<h1 class="text-center">My Profile</h1>
     <div class="col-md-12">
-    <p><b>Full Name:</b></p>$me['name']</p>
-    <p><b>Controller ID:</b></p>$me['cid']</p>
-    <p><b>Operating Initials:</b></p>$me['operating_id']
-    <p><b>Email:</b></p><?php $me['email'] = Form::text('email', 'example@gmail.com');?>
-    Form::checkbox('Subscribe', 'value', true);
+    <form action="/u/{{$me->cid}}/settings/update" method="post">
+	    <p><b>Full Name:</b> {{ $me->first_name . ' ' . $me->last_name }}</p>
+	    <p><b>Controller ID:</b> {{ $me->cid }}</p>
+	    <p><b>Operating Initials:</b> {{ $me->initials }}
+		<div class="input-group">
+			{{ Form::label('email', 'Email'); }}
+			<input type="email" name="email" id="email" value="{{$me->email}}" class="form-control">
+		</div>
+		{{ Form::checkbox('Subscribe', 'subscribe', true); }}
+		<button type="submit" class="btn btn-primary">Update</button>
+	    
+    </form>
 @stop
