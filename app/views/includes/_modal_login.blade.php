f153df222fa4f803a47cb9fc0bbed9b4dfd30778
mikedugan <mike@mjdugan.com>
2014-05-14 18:54:29 -0400

    login update

--- app/views/includes/_modal_login.blade.php
+++ app/views/includes/_modal_login.blade.php
@@ -5,33 +5,33 @@
         <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
         <h4 class="modal-title" id="myModalLabel">vZBW Login</h4>
       </div>
+      <div class="modal-body">
       <div class = "col-md-6">
-        <div class="modal-body">
         <form action="/login" method="post">
-        	<div class="input-group col-md-6">
+        	<div class="input-group">
         		<label for="username">Username</label>
         		<input class="form-control" type="text" name="username" id="username">
         	</div>
-        	<div class="input-group col-md-6">
+        	<div class="input-group">
         		<label for="password">Password</label>
         		<input class="form-control" type="password" name="password" id="password">
         	</div>
-        	<div class="input-group col-md-6">
+        	<div class="input-group">
       			<span class="input-group-addon">
         			<input type="checkbox">
       			</span>
       			<input value="Remember Me" type="text" class="form-control">
     		</div><!-- /input-group -->
       </div>
-      </div>
       <div class = "col-md-6">
           <img src= 'images/zbw_logo.png'>
       </div>
-      <div class="modal-footer">
+      <div class="modal-footer text-left">
         <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
         <button type="submit" class="btn btn-primary">Login</button>
         </form>
       </div>
     </div>
+  </div>
   </div>
 </div>
