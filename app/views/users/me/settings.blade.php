6e3d0ab9e83cfb8bbbe578b7c96f3865205fe040
mikedugan <mike@mjdugan.com>
2014-05-31 16:56:38 -0400

    adapted teamspeak manager to eloquent

--- app/views/users/me/settings.blade.php
+++ app/views/users/me/settings.blade.php
@@ -6,28 +6,36 @@ My Profile
 @stop
 @section('content')
 	<h1 class="text-center">My Profile</h1>
-    <div class="col-md-12">
+    <div class="col-md-10 col-md-offset-2">
     <form action="/u/{{$me->cid}}/settings/update" method="post">
-	    <p><b>Full Name:</b> {{ $me->first_name . ' ' . $me->last_name }}</p>
-	    <p><b>Controller ID:</b> {{ $me->cid }}</p>
-	    <p><b>Operating Initials:</b> {{ $me->initials }}
-		<div class="input-group">
-			{{ Form::label('email', 'Email'); }}
-			<input type="email" name="email" id="email" value="{{$me->email}}" class="form-control">
-		</div>
-		<p><b>Subscribed:</b></p>
-        @if($me->is_subscribed)
-        <form class="axform" action="path/to/subscribe" method="post">
-            <button type="submit">Subscribe</button>
-        </form>
-        @else
-        <form class="axform" action="path/to/unsubscribe" method="post">
-            <button type="submit">Unsubscribe</button>
-        </form>
-        @endif
-        <p><b>Email Hidden:</b></p>
-        {{ Form::checkbox('Hidden', 'hidded', false); }}
-        <p><b>Signature</b></p> {{ $me->signature}}
+        <div class="col-md-6">
+            <p><b>Full Name:</b> {{ $me->first_name . ' ' . $me->last_name }}</p>
+            <p><b>Controller ID:</b> {{ $me->cid }}</p>
+            <p><b>Operating Initials:</b> {{ $me->initials }}
+            <div class="input-group">
+                {{ Form::label('email', 'Email'); }}
+                <input type="email" name="email" id="email" value="{{$me->email}}" class="form-control">
+            </div>
+            <p><b>Subscribed:</b></p>
+            @if($me->is_subscribed)
+            <form class="axform" action="path/to/subscribe" method="post">
+                <button type="submit">Subscribe</button>
+            </form>
+            @else
+            <form class="axform" action="path/to/unsubscribe" method="post">
+                <button type="submit">Unsubscribe</button>
+            </form>
+            @endif
+            <p><b>Email Hidden:</b></p>
+            {{ Form::checkbox('Hidden', 'hidden', false); }}
+            <p><b>Signature</b></p> {{ $me->signature}}
+        </div>
+        <div class="col-md-6">
+            <div class="input-group">
+                <label class="label-control" for="tskey">Teamspeak Key</label>
+                <input class="form-control" name="tskey" id="tskey" type="text">
+            </div>
+        </div>
 		<button type="submit" class="btn btn-primary">Update</button>
     </form>
     </div>
