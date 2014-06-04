8b88332176882850842dd0cbf2d0ae380ad8c23e
mikedugan <mike@mjdugan.com>
2014-06-03 21:53:49 -0400

    work on user setting/profile views

--- app/views/users/me/profile.blade.php
+++ app/views/users/me/profile.blade.php
@@ -3,7 +3,31 @@
     <p><b>Full Name:</b> {{ $me->first_name . ' ' . $me->last_name }}</p>
     <p><b>Controller ID:</b> {{ $me->cid }}</p>
     <p><b>Operating Initials:</b> {{ $me->initials }}</p>
+    <p><b>Email:</b> {{ $me->email }}</p>
 
+    <p><b>Staff Positions:</b><br/>
+    @if($me->is_atm)
+        ATM <br>
+    @endif
+    @if($me->is_datm)
+        DATM <br>
+    @endif
+    @if($me->is_ta)
+        TA <br/>
+    @endif
+    @if($me->is_webmaster)
+        Webmaster <br/>
+    @endif
+    @if($me->is_fe)
+        FE <br/>
+    @endif
+    @if($me->is_instructor)
+        Instructor
+    @endif
+    @if($me->is_mentor)
+        Mentor
+    @endif
+    </h4>
 </div>
 <div class="col-md-6">
     <h3>Recent Exams</h3>
