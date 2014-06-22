594ff499acdefe612e4ad97a1debf38d4d6f28e7
mikedugan <mike@mjdugan.com>
2014-06-22 13:14:15 -0400

    added gulp.js and minification

--- app/views/includes/_head.blade.php
+++ app/views/includes/_head.blade.php
@@ -1,4 +1,5 @@
 {{ HTML::style('css/bootstrap.css') }}
+{{ HTML::style('css/glyphicons.css') }}
 {{ HTML::style('css/jquery.datetimepicker.css') }}
 {{ HTML::style('js/redactor/redactor.css') }}
 {{ HTML::style('css/custom.css') }}
