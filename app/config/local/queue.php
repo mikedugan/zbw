79409e73a09e52db3fa9dc94ba7582f83b6ce9a9
mikedugan <mike@mjdugan.com>
2014-06-24 19:09:36 -0400

    added basic support for queues

--- app/config/local/queue.php
+++ app/config/local/queue.php
@@ -1,5 +1,18 @@
 <?php
 
 return [
-
+    'default' => 'beanstalkd',
+    'connections' => [
+      'sync' => [
+        'driver' => 'sync',
+      ],
+      'beanstalkd' => [
+        'driver' => 'beanstalkd',
+        'host'   => 'localhost',
+        'queue'  => 'default',
+      ],
+    ],
+    'failed' => [
+      'database' => 'mysql', 'table' => 'failed_jobs'
+    ]
 ];
