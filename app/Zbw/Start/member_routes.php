b4cb172f173911f995cc13c9461010084941d968
mikedugan <mike@mjdugan.com>
2014-06-28 22:56:06 -0400

    name all the routes!

--- app/Zbw/Start/member_routes.php
+++ app/Zbw/Start/member_routes.php
@@ -1,31 +1,111 @@
 <?php
 
 Route::group(
-  array('before' => 'controller'), function () {
+  array('before' => 'controller'),
+  function () {
       $cid = is_null(Auth::user()) ? 0 : Auth::user()->cid;
       //routes for the logged in users
-      Route::get('me', 'ControllersController@getMe');
-      Route::post('/me/markallread', 'AjaxController@markInboxRead');
-      Route::get('/u/' . $cid,array('as' => 'me', 'uses' => 'ControllersController@getMe'));
-      Route::get('me/profile', 'ControllersController@getSettings');
+      Route::post(
+        '/me/markallread',
+        ['as' => '/me/markallread', 'uses' => 'AjaxController@markInboxRead']
+      );
+      Route::get(
+        '/u/' . $cid,
+        array('as' => 'me', 'uses' => 'ControllersController@getMe')
+      );
+      Route::get(
+        'me/profile',
+        ['as' => 'profile', 'uses' => 'ControllersController@getSettings']
+      );
+      Route::post(
+        'me/profile',
+        ['as' => 'profile', 'uses' => 'ControllersController@postSettings']
+      );
       //training and exam routes
-      Route::get('training/request/new', 'TrainingController@getRequest');
-      Route::get('training/request/{id}', 'TrainingController@showRequest');
-      Route::get('/training/review', 'TrainingController@getReview');
-      Route::post('/training/review/{eid}', 'ControllerExamsController@postComment');
-      Route::post('/e/request/{cid}/{eid}',array('uses' => 'AjaxController@requestExam'));
-      Route::post('/t/request/new', 'AjaxController@postTrainingRequest');
-      Route::get('/t/request/{tid}', 'TrainingController@showRequest');
-      Route::post('/t/request/{tid}/cancel','AjaxController@cancelTrainingRequest');
-      Route::post('/t/request/{tid}/accept','AjaxController@acceptTrainingRequest');
-      Route::post('/f/upload/photos', 'AjaxController@photoUpload');
+      Route::get(
+        'training/request/new',
+        [
+          'as'   => 'training/new-request',
+          'uses' => 'TrainingController@getRequest'
+        ]
+      );
+      Route::get(
+        'training/request/{id}',
+        [
+          'as'   => 'training/view-request/{id}',
+          'uses' => 'TrainingController@showRequest'
+        ]
+      );
+      Route::get(
+        '/training/review',
+        ['as' => 'training/sessions', 'uses' => 'TrainingController@getReview']
+      );
+      Route::post(
+        '/training/review/{eid}',
+        [
+          'as'   => 'training/review-session',
+          'uses' => 'ControllerExamsController@postComment'
+        ]
+      );
+      Route::post(
+        '/e/request/{cid}/{eid}',
+        [
+          'as'   => 'me/exam-requests/{eid}',
+          'uses' => 'AjaxController@requestExam'
+        ]
+      );
+      Route::post(
+        '/t/request/new',
+        [
+          'as'   => 'me/request-training',
+          'uses' => 'AjaxController@postTrainingRequest'
+        ]
+      );
+      Route::get(
+        '/t/request/{tid}',
+        [
+          'as'   => 'training/view-request/{tid}',
+          'uses' => 'TrainingController@showRequest'
+        ]
+      );
+      Route::post(
+        '/t/request/{tid}/cancel',
+        [
+          'as'   => 'training/cancel-request/{tid}',
+          'uses' => 'AjaxController@cancelTrainingRequest'
+        ]
+      );
+      Route::post(
+        '/t/request/{tid}/accept',
+        [
+          'as'   => 'training/accept-request/{tid}',
+          'uses' => 'AjaxController@acceptTrainingRequest'
+        ]
+      );
       //private messaging
       Route::group(
-        ['prefix' => 'messages'], function () {
-            Route::get('/', 'MessagesController@index');
-            Route::get('m/{mid}', 'MessagesController@view');
-            Route::get('m/{mid}', 'MessagesController@view');
-            Route::post('send', 'MessagesController@store');
-            Route::post('m/{mid}', 'MessagesController@reply');
-        });
-  });
+        ['prefix' => 'messages'],
+        function () {
+            Route::get(
+              '/',
+              ['as' => 'messages', 'uses' => 'MessagesController@index']
+            );
+            Route::get(
+              'm/{mid}',
+              ['as' => 'messages/{mid}', 'uses' => 'MessagesController@view']
+            );
+            Route::post(
+              'send',
+              ['as' => 'messages/send', 'uses' => 'MessagesController@store']
+            );
+            Route::post(
+              'm/{mid}',
+              [
+                'as'   => 'messages/reply/{mid}',
+                'uses' => 'MessagesController@reply'
+              ]
+            );
+        }
+      );
+  }
+);
