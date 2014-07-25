035082380972fe994025d875ab675b0d11dfcbdd
mikedugan <mike@mjdugan.com>
2014-07-25 17:53:33 -0400

    fixes #44

--- app/views/includes/nav/_public_roster.blade.php
+++ app/views/includes/nav/_public_roster.blade.php
@@ -2,6 +2,5 @@
     <ul class="nav nav-tabs">
         <li class="<?php if(! $view) echo 'active'; ?>"><a href="/roster">Roster</a></li>
         <li class="<?php if($view === 'staff') echo 'active'; ?>"><a href="?v=staff">Staff</a></li>
-        <li class="<?php if($view === 'groups') echo 'active'; ?>"><a href="?v=groups">Groups</a></li>
     </ul>
 </div>
