512611e3ac70df3413e593fce0e7f45091da368f
mikedugan <mike@mjdugan.com>
2014-07-25 17:32:42 -0400

    added changelog

---
+++ app/views/zbw/changelog.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.master')
+@section('title')
+Changelog
+@stop
+@section('content')
+<h1 class="text-center">Changelog</h1>
+<h3>v2.0.0b</h3>
+mikedugan fixed roster import bug and a couple issues with roster views<br>
+mikedugan typo<br>
+mikedugan fixes #28 #29<br>
+mikedugan damned queues<br>
+mikedugan fixed profile information display<br>
+mikedugan fixed training session and staffing view<br>
+mikedugan numerous fixes and updates to training and permissions<br>
+mikedugan fixed 403 pages<br>
+@stop
