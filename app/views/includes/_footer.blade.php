a4bf5dd67fc5e20ed92d990996ddb3760581f835
mikedugan <mike@mjdugan.com>
2014-08-09 19:29:48 -0400

    changelog

--- app/views/includes/_footer.blade.php
+++ app/views/includes/_footer.blade.php
@@ -1,6 +1,6 @@
 _<p><a style="cursor:pointer" data-toggle="modal" data-target="#errorModal">Report Error</a> | <span class="left">Content &copy; 2014 vZBW ARTCC</span>
     <span class="right"><a href="#">VATSIM Disclaimer</a></span>
-    | v2.0.7b
+    | v2.0.8b
  <a style="display:none;cursor:pointer" data-toggle="modal" data-target="#feedbackModal">Feedback</a>
     {{--@include('includes.forms.feedback')--}}
     @include('includes.forms.error')
