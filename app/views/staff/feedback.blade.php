d4405a6ae182cdfe13f4ed0ce98101385f09b6db
Mike Dugan <mike@mjdugan.com>
2014-10-11 17:52:02 -0400

    update view

--- app/views/staff/feedback.blade.php
+++ app/views/staff/feedback.blade.php
@@ -21,7 +21,7 @@ View Feedback
                 <td>{{ $feedback->name }}</td>
                 <td><a href="mailto:{{$feedback->email}}">{{ $feedback->email }}</a></td>
                 <td>{{ Zbw\Core\Helpers::pilotFeedbackRating($feedback->rating) }}</td>
-                <td>{{ \User::whereCid($feedback->cid)->first()->initials }}</td>
+                <td>{{ \User::whereCid($feedback->controller)->first()->initials }}</td>
                 <td>{{ $feedback->response == 1 ? 'Yes' : 'No' }}</td>
                 <td>{{ $feedback->created_at->toDayDateTimeString() }}</td>
                 <td style="max-width:250px;">{{ $feedback->comments }}</td>
