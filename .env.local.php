b0c6ce5abaa1ce03853e9966d1d8668147efbb29
Mike Dugan <mike@mjdugan.com>
2015-03-21 08:28:59 -0400

    merge L5 base

---
+++ .env.local.php
@@ -0,0 +1,49 @@
+<?php
+
+return [
+  'db_name' => 'zbw_laravel',
+  'old_db_name' => 'zbw_old',
+  'db_user' => 'root',
+  'db_pass' => 'root',
+  'db_host' => 'localhost',
+
+  'vatid'     => 4,
+  'vatkey'    => 'ahwwSh8N0BlobHcuzcozmbZ2me5TCs',
+
+
+  'sso_key' => 'BOSTON_ARTCC',
+  'sso_secret' => 'YPcil3f4QpsNl48jlzQ',
+  'sso_cert' => '-----BEGIN RSA PRIVATE KEY-----
+MIIEowIBAAKCAQEAm+QGWu3l7ed18AmmtJYthT8IKKjpGj8Re0ZDU/Mi9ZpaH3Pv
+C7m3mNS+z3fUNcWlpmdjOC6gnpZFevFpDQs4nOV91mpvlMz7ezjqfhJ2xGqNFP1+
+xjMh2xkzN/Xk6wSFUOxUgqeviPKzudN8M1k9oiHir1U7oGsg8tFks2K95MIwDoVm
+FYg8MxaGtxi83uD6zjH/Y7+CJMxK1NZO1uplUyGjXcV2L8RK1vvmmk5SrC9phb8Z
+zl4Yel/zqhtNmKJXk5kFhMwuFBizQHRg+p/BJ+QvGMayDgyiyeam/hMtPr9ji3r/
+EM/FpoOl9K/fKojXvjEXmc3asS5wqprhKPaUEwIDAQABAoIBAAsVp64HhHXqI2Sy
+CvyEQTBcTwQ1Hn4TRfbyBA+QFkhIdWgPIkw/JGLlnbu61YgNSb4PU+rGxf0Aymf3
+inS2+4uMdeotH0oBWVBlt6bHQVKuEOIRNFZ7hEjD/fRYs7JSW/hpFYlEyyJ9v3HT
+K92q1ssUhb3Z3aeGdo1Nfk+TEdWWLNNAwFy7KeFpkZZGVYjAJdMV1Vg/V1bYjfu+
+ZjOaV7/Wg3TizZX3OK6rvVm3c2IWQ1YPkzY0anB5zIMUA4x1KCLAKZAbQ/G6dIE3
+VnUNjMYeFrxd+XzTVVVBvs24zSxAb34bib0h8yeOpgCAfrdOt5QlKI1dHUUbSiUN
+CBm54oECgYEAyhRbEwrD6BS0vXop0gIlEQJdyZ/RI/4lUzblMr6zPG1RHAeniVKn
+pKI6aQ1nuRh5xNZESsFIAYaPqEgPgtGaBFwp/avrERkA3074bBAsD7NwIlHlyIj2
+pIbkbBj2+l+S6O/INNSozLp6c62x6d5P7SZ/Yq/n89KkgfM36UCsWmMCgYEAxXyb
+Esu6E7PWTfiZAjSJ4+fBSd4K9P5x9ri6ufq9dT+Qn1JB08VV20ehaCfGdQPoU1zq
+lnrCHNYipMUe/DGRoatw/blpIYpz/0ApIbjKK96mw3e7dD5IopjCSBvZBKsBpKi3
+absuNUS2fNESqC3RstcoxND5wPOadE0Tk/4ntpECgYBjVg5PFgOnmkYevmbAbX17
+FwazjnRolQ4UEfApOcUx9yrf9INO5eqy62x7J7PNMVLqh8maVdjnpxYHSyUU0aQ8
+cHHT2Sb8iKX9H/zOYkTGkxaUSF4Ebg+QYR74ZG1ENqcpCnYIQmGYAjMfmkrTBA2W
+Lk6AXKhRG0Ly/nSFnN0csQKBgDBHtEm2ELm8X4oUWqPVYFidUf3TEO6wPgBJTS0m
+habicWLh40H4nnLLYkRqZPQ6jlPDBX7gaBBQsYuCvX5+R9I0bZ059N/ET3N5phSe
+bp6nAjILVYKnr25A1k52sGmZvP8aWCy7HCLdt5dwLZV8DiYgv9DOHiDz7TC9nymx
+GthhAoGBALbSbQvGy7Io1krrYffKKawbUgnM8BxxRLEEmpb4Kfa+p/x/DfowuhoJ
+xCpdEgEOJp+kxyYALvsBFAp7X8AOQ5j7JRZdHAPs+nNe4Wr9ZVXBKze8UYu6JVEy
+NOTbVosifFQhuZ0fRgHP2QO4hCeuqMvvR4G5GhXABnLiIU1sJhVZ
+-----END RSA PRIVATE KEY-----',
+
+  'ts_host' => '192.241.125.73',
+  'ts_user' => 'serveradmin',
+  'ts_pass' => 'Mickeyd2!',
+  'ts_queryport' => '10011',
+  'ts_port' => '9987'
+];
