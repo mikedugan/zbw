18896bf6dd5c044ff31001ab8e7599557173ca10
Mike Dugan <mike@mjdugan.com>
2015-03-20 21:54:42 -0400

    update sso config

--- app/config/production/zbw.php
+++ app/config/production/zbw.php
@@ -1,9 +1,9 @@
 <?php
 return [
     'sso' => [
-        'key' => $_ENV['sso_key'],
-        'secret' => $_ENV['sso_secret'],
-        'cert' => $_ENV['sso_cert'],
+        'key' => getenv('sso_key'),
+        'secret' => getenv('sso_secret'),
+        'cert' => getenv('sso_cert'),
         'method' => 'RSA',
         'return' => 'http://bostonartcc.net/auth?return',
         'base' => 'https://cert.vatsim.net/sso/'
