73a4f2e23518ca894ab7c129177338e855355127
Mike Dugan <mike@mjdugan.com>
2015-03-21 13:07:40 -0400

    update

--- resources/views/includes/_head.blade.php
+++ resources/views/includes/_head.blade.php
@@ -1,4 +1,4 @@
 <link rel="stylesheet" href="/dist/zbw.min.css"/>
 <link rel="stylesheet" href="/styles/font-awesome.min.css"/>
 <link rel="stylesheet" href="/styles/froala_editor.min.css"/>
-<link href='http://fonts.googleapis.com/css?family=Noto+Sans|Pontano+Sans' rel='stylesheet' type='text/css'>
+<link href='https://fonts.googleapis.com/css?family=Noto+Sans|Pontano+Sans' rel='stylesheet' type='text/css'>
